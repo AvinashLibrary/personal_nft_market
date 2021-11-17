fe7efb7073f9144a267b611053c50c8fc5907f6f
avinashKumar <avinash@Kumar>
2021-11-17 22:09:40 +0530

    testing code completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,12 @@ require('@nomiclabs/hardhat-waffle');
 const secret = require('./environment/secret.json');
 module.exports = {
   solidity: "0.8.4",
-  paths: {
-    sources: './blockchain/contracts',
-    tests: './blockchain/test',
-    cache: './blockchain/scripts',
-    artifacts: './blockchain/artifacts',
-  },
+  // paths: {
+  //   sources: './blockchain/contracts',
+  //   tests: './blockchain/test',
+  //   cache: './blockchain/scripts',
+  //   artifacts: './blockchain/artifacts',
+  // },
   networks: {
     hardhat: {
       chainId: 1337
